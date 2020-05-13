654b4e3accae9ddd78c60e384072d5b052048a34
Ashish SIngh <ashsihs41@gmail.com>
2020-05-13 22:35:59 +0530

    indentations added

--- placementsuccess/js/deleteJob.js
+++ placementsuccess/js/deleteJob.js
@@ -8,58 +8,55 @@ function deleteJob() {
 	// body...
 	const deleteJobForm = document.querySelector("#deleteJob_form");
 	const jobKey = deleteJobForm["jobKey"].value.trim();
-	if(jobKey){
-		firebase.database().ref("Company/"+jobKey).once('value',   function(snap) {
-	  	if (snap.val()!=null) {
-	  		document.getElementById("search").style.display = "none";
+	if (jobKey) {
+		firebase.database().ref("Company/" + jobKey).once('value', function (snap) {
+			if (snap.val() != null) {
+				document.getElementById("search").style.display = "none";
 
-	  		var childData = snap.val();
-	    	var key=jobKey;
-	      	var jobTitle=childData['jobTitle'];
-	      	var requiredSkills=childData['requiredSkills'];
-	      	var jobCTC=childData['jobCtc'];
-	      	var applyingDateFrom=childData['applyDateFrom'];
-	      	var applyingDateTo=childData['applyDateTo'];
-	      	var dataSet = [key,jobTitle,requiredSkills,jobCTC,applyingDateFrom,applyingDateTo];
-	      	$("#myTable tbody").append(
-			      "<tr>" +
-			        "<td>"+dataSet[0]+"</td>" +
-			        "<td>"+dataSet[1]+"</td>" +
-			        "<td>"+dataSet[2]+"</td>" +
-			        "<td>"+dataSet[3]+"</td>" +
-			        "<td>"+dataSet[4]+"</td>" +
-			        "<td>"+dataSet[5]+"</td>" +
-			      "</tr>");
-	  	}
-	  	else{
-	  		$("#myTable tbody").append(
-			      "<tr>" +
-			        "<td>Sorry, No Data Found</td>" +
-			      "</tr>");
-	  		document.getElementById("delete").style.display = "none";
-	  		document.getElementById("cancel").style.display = "none";
-	  	}
+				var childData = snap.val();
+				var key = jobKey;
+				var jobTitle = childData['jobTitle'];
+				var requiredSkills = childData['requiredSkills'];
+				var jobCTC = childData['jobCtc'];
+				var applyingDateFrom = childData['applyDateFrom'];
+				var applyingDateTo = childData['applyDateTo'];
+				var dataSet = [key, jobTitle, requiredSkills, jobCTC, applyingDateFrom, applyingDateTo];
+				$("#myTable tbody").append(
+					"<tr>" +
+					"<td>" + dataSet[0] + "</td>" +
+					"<td>" + dataSet[1] + "</td>" +
+					"<td>" + dataSet[2] + "</td>" +
+					"<td>" + dataSet[3] + "</td>" +
+					"<td>" + dataSet[4] + "</td>" +
+					"<td>" + dataSet[5] + "</td>" +
+					"</tr>");
+			} else {
+				$("#myTable tbody").append(
+					"<tr>" +
+					"<td>Sorry, No Data Found</td>" +
+					"</tr>");
+				document.getElementById("delete").style.display = "none";
+				document.getElementById("cancel").style.display = "none";
+			}
 
-	  	document.getElementById('information').style.display="block";
-    	});
-	}
-	else{
+			document.getElementById('information').style.display = "block";
+		});
+	} else {
 		alert("Please Enter the key!!!");
 	}
 }
 
-function cancel(){
+function cancel() {
 	window.location.reload();
 }
 
-function deleteJobFromDb(){
+function deleteJobFromDb() {
 
-	var t = confirm("Do you really want to Delete?");       
-    if(t == true)    
-    {    
-    	const deleteJobForm = document.querySelector("#deleteJob_form");
-    	const jobKey = deleteJobForm["jobKey"].value.trim();
-    	firebase.database().ref("Company/"+jobKey).remove();
-    	window.location.reload();
-    }    
+	var t = confirm("Do you really want to Delete?");
+	if (t == true) {
+		const deleteJobForm = document.querySelector("#deleteJob_form");
+		const jobKey = deleteJobForm["jobKey"].value.trim();
+		firebase.database().ref("Company/" + jobKey).remove();
+		window.location.reload();
+	}
 }
